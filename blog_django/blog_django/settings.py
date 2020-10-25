03c52ae8a87f543af6b75817067e7bb25459d96a
knowrafa <noprafa@gmail.com>
2020-10-25 14:53:46 -0300

    Primeiras páginas

--- blog_django/blog_django/settings.py
+++ blog_django/blog_django/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_DIR= os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'blog_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['%s/templates/' % (PROJECT_DIR)],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
